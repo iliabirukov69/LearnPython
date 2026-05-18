fb5ae127921fe1d3a7f984fce10c5b9cf4d84c5e
iliabirukov69 <ilabirukov69@gmail.com>
2026-05-18 14:19:56 +0200

    Create 05_songs.py

---
+++ hw3/05_songs.py
@@ -0,0 +1,14 @@
+violator_songs = [["World in My Eyes", 4.86], ["Sweetest Perfection", 4.43], ["Personal Jesus", 4.56],
+    ["Halo", 4.9], ["Waiting for the Night", 6.07], ["Enjoy the Silence", 4.20], ["Policy of Truth", 4.76],
+    ["Blue Dress", 4.29], ["Clean", 5.83]]
+
+count = int(input("Сколько песен выбрать? "))
+total_time = 0
+for i in range(count):
+    song_name = input(f"Название {i + 1}-й песни: ")
+    for song, duration in violator_songs:
+        if song == song_name:
+            total_time += duration
+            break
+
+print(f"\nОбщее время звучания песен: {round(total_time, 2)} минуты" )
